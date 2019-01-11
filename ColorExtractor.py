12e7bf24bcc98f51c62016c1a77d0ea6043d8277
MaloPc <malo.maisonneuve@gmail.com>
2019-01-11 18:01:46 +0100

    Creating a photo class, needs to be completed

--- ColorExtractor.py
+++ ColorExtractor.py
@@ -4,6 +4,12 @@ from PIL import Image
 
 sat = 0
 hue = 0
+value = 0
+
+# We define the range of degrees for HSV, ignoring 360
+colors_ref = []
+for i in range(360):
+    colors_ref.append(i)
 
 # First, we get the list of the pictures
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -13,6 +19,7 @@ file_list = os.listdir(data_path) # file_list is an array
 # We define the variables we will used to store our colors
 pixcolor = []
 color_list = []
+color_list_hsv = []
 
 for i in range(len(file_list)):
     pixcolor = []
@@ -24,40 +31,52 @@ for i in range(len(file_list)):
     # We store the color of each pixel in pixcolor and then in color_list
     for i in range(2):
         for j in range(2):
-            #print(pixels[i,j])
+            # We store it in RGB first just in case
             pixcolor.append(pixels[i,j])
+            # We now store it in HSV
+            r = pixels[i,j][0]/255
+            g = pixels[i,j][1]/255
+            b = pixels[i,j][2]/255
+            Cmax = max(r, g, b)
+            Cmin = min(r, g, b)
+            delta = Cmax - Cmin
+
+            hue = 0
+            sat = 0
+            value = Cmax
+
+            if Cmax == r:
+                hue = 60*((g-b)/delta % 6)
+            elif Cmax == g:
+                hue = 60*((b-r)/delta + 2)
+            elif Cmax == b:
+                hue = 60*((r-g)/delta + 4)
+
+            if Cmax != 0:
+                sat = delta/Cmax
+
+            color_list_hsv.append(hue)
+            color_list_hsv.append(sat)
+            color_list_hsv.append(value)
 
     color_list.append(pixcolor)
-    print(color_list)
+print(color_list)
+print('hsv =', color_list_hsv)
 
     # We create a list of the colors converted from rgb to hsv without the value
 
-
-
-
 os.system("pause")
 
+class photo:
+    """This class represents a photo, with its four dominant colors in RGB
+    and in HSV and its position amoungst the color reference"""
 
-def hue(R, G, B):
-    r = R/255
-    g = G/255
-    b = B/255
-    Cmax = max(r, g, b)
-    Cmin = min(r, g, b)
-    delta = Cmax - Cmin
+    colors_ref = []
+    for i in range(360):
+        colors_ref.append(i
+        
 
-    H = 0
-    S = 0
-
-    if Cmax == r:
-        H = 60*((g-b)/delta % 6)
-    elif Cmax == g:
-        H = 60*((b-r)/delta + 2)
-    elif Cmax == b:
-        H = 60*((r-g)/delta + 4)
-
-    if Cmax != 0:
-        S = delta/Cmax
-
-    sat = S
-    hue = H
+    def __init__(self, red, blue, green):
+        self.red = red
+        self.blue = blue
+        self.green = green
