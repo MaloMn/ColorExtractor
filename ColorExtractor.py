5c5ffbf6f98d4f605784910722744224f5cb7b15
MaloPc <malo.maisonneuve@gmail.com>
2019-01-12 20:43:25 +0100

    py file modified to resize photo to 1*1

--- ColorExtractor.py
+++ ColorExtractor.py
@@ -18,47 +18,52 @@ class photo:
     def __init__(self, path):
         """We construct the object by assigning to it 4 dominant colors in
         RGB and HSV"""
+
+        print(path)
+
         self.path = path
         self.colors_rgb = []
         self.colors_hsv = []
         #print("[{}] {}".format(i, file_list[i]))
         # We load one image, resize it to a 2x2,
         img = Image.open('data/' + self.path)
-        img = img.resize((2,2), Image.ANTIALIAS)
+        img = img.resize((1,1), Image.ANTIALIAS)
         pixels = img.load()
         # We store the color of each pixel in pixcolor and then in color_list
-        for i in range(2):
-            for j in range(2):
-                # We store one color in RGB first just in case
-                # so it has the r, g and b components
-                for k in range(len(pixels[i,j])):
-                    self.colors_rgb.append(pixels[i,j][k])
-                # We now store it in HSV
-                r = pixels[i,j][0]/255
-                g = pixels[i,j][1]/255
-                b = pixels[i,j][2]/255
-                Cmax = max(r, g, b)
-                Cmin = min(r, g, b)
-                delta = Cmax - Cmin
+        # We store one color in RGB first just in case
+        # so it has the r, g and b components
+        for k in range(len(pixels[0,0])):
+            self.colors_rgb.append(pixels[0,0][k])
+        # We now store it in HSV
+        r = pixels[0,0][0]/255
+        g = pixels[0,0][1]/255
+        b = pixels[0,0][2]/255
+        Cmax = max(r, g, b)
+        Cmin = min(r, g, b)
+        delta = Cmax - Cmin
 
-                hue = 0
-                sat = 0
-                value = Cmax
+        hue = 0
+        sat = 0
+        value = Cmax
 
-                if Cmax == r:
-                    hue = 60*((g-b)/delta % 6)
-                elif Cmax == g:
-                    hue = 60*((b-r)/delta + 2)
-                elif Cmax == b:
-                    hue = 60*((r-g)/delta + 4)
+        if Cmax == r and delta != 0:
+            hue = 60*((g-b)/delta % 6)
+        elif Cmax == g and delta != 0:
+            hue = 60*((b-r)/delta + 2)
+        elif Cmax == b and delta != 0:
+            hue = 60*((r-g)/delta + 4)
 
-                if Cmax != 0:
-                    sat = delta/Cmax
-                # We add the same color in hsv to the hsv list
-                self.colors_hsv.append(hue)
-                self.colors_hsv.append(sat)
-                self.colors_hsv.append(value)
+        if Cmax != 0:
+            sat = delta/Cmax
+        # We add the same color in hsv to the hsv list
+        self.colors_hsv.append(hue)
+        self.colors_hsv.append(sat)
+        self.colors_hsv.append(value)
         # Now we have two lists with 3*4 elements each.
+
+    def __str__(self):
+        """Showing the path a photo"""
+        return self.path
 
 # First, we get the list of the pictures
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -70,7 +75,13 @@ photos_list = []
 for i in range(len(file_list)):
     photos_list.append(photo(file_list[i]))
 
-print(photos_list[0].colors_hsv)
+photos_list.sort(key= lambda photo: photo.colors_hsv[0])
+
+for i in range(len(photos_list)):
+    print(photos_list[i])
+    new = Image.open('data/' + photos_list[i].path)
+    new.save('result/' + str(i) + '.bmp')
+
 
 # We create a list of the colors converted from rgb to hsv without the value
 
