213268e2e4c8c7a449e72542522fe46c63a877b4
MaloMn <malo.maisonneuve@gmail.com>
2019-09-03 10:51:17 +0200

    Code reorganized according to PEP8.

--- c_photo.py
+++ c_photo.py
@@ -2,7 +2,8 @@ from functions import *
 from PIL import Image
 from sklearn.cluster import DBSCAN
 import numpy as np
- 
+
+
 class Photo:
     
     def __init__(self, path):    
@@ -11,25 +12,27 @@ class Photo:
             It also lists them in self.pixels_list.
         """
         self.path = path
-             
         self.pixels = np.asarray(Image.open(self.path))
         
         # We list the pixels in an array of dimension 2
         self.pixels_list = []
         for i in range(0, len(self.pixels), 1):
             for j in range(0, len(self.pixels[0]), 1):
-                self.pixels_list.append(RGBtoHSB(tuple(self.pixels[i,j].tolist())))
-                
+                self.pixels_list.append(RGBtoHSB(tuple(self.pixels[i, j].tolist())))
+
+        self.nb_wished_colors, self.hue_rez, self.sat_rez, self.val_rez = (0, 0, 0, 0)
+        self.colors = []
+
     def compute(self, nb_wished_colors, hue_rez, sat_rez, val_rez):
         """
             To get the main colors, we plot virtually the colors in a 3d space, 
             and we cut some smaller cubes in that space. The cubes containing 
             most points are kept.
         """
-        self.nb_wished_colors = nb_wished_colors #= 16
-        self.hue_rez = hue_rez #= 20   # 0 <= hue < 360
-        self.sat_rez = sat_rez #= 0.075 # 0 <= saturation <= 1
-        self.val_rez = val_rez #= 0.075 # 0 <= value <= 1
+        self.nb_wished_colors = nb_wished_colors
+        self.hue_rez = hue_rez  # 0 <= hue < 360
+        self.sat_rez = sat_rez  # 0 <= saturation <= 1
+        self.val_rez = val_rez  # 0 <= value <= 1
         
         repartition = np.zeros((int(360/hue_rez), int(1/sat_rez), int(1/val_rez)), dtype=int)
         
@@ -42,8 +45,7 @@ class Photo:
             repartition[int(hue/self.hue_rez), int(sat/self.sat_rez), int(val/self.val_rez)] += 1
         
         # Collecting the fourth most important colors
-        self.colors = []
-        
+
         def generator():
             for x in range(len(repartition)):
                 for y in range(len(repartition[0])):
@@ -60,7 +62,7 @@ class Photo:
                     repartition[x,y,z] = 0
                     break
                 
-        #self.colors now contains the most important colors.
+        # self.colors now contains the most important colors.
     
     def save(self, name):
         """
@@ -74,7 +76,7 @@ class Photo:
         
         for x in range(width):
             for y in range(width):
-                index = factor * (x // size) + (y // size) #x+factor*y
+                index = factor * (x // size) + (y // size)  # x+factor*y
                 
                 hue = self.colors[index][0]
                 sat = self.colors[index][1]
@@ -93,6 +95,6 @@ class Photo:
             output.append(tuple(grid[i]))
         
         from png import Writer
-        with open('output.png', 'wb') as f:
+        with open('o_' + name + '.png', 'wb') as f:
             w = Writer(width, width, greyscale=False, alpha=False)
             w.write(f, output)
